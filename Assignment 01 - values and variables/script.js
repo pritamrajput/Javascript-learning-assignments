0ed15dc611c05151e77539b76ec57ba2dc0af5e9
Pritam Chauhan <rajputpritam619@gmail.com>
2022-12-13 22:14:43 +0530

    assignment 01 added

---
+++ Assignment 01 - values and variables/script.js
@@ -0,0 +1,33 @@
+// 1.) This is in the class
+// let js = 'amazing';
+// console.log(40+2+45-32);
+
+// console.log("jonas");
+// console.log(23);
+
+// let firstName = "Pritam";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let pritam_chauhan = "PC";
+// let $function = 27;
+
+// let person = 'jonas';
+// let PI = 3.1415;
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+
+// console.log(myFirstJob);
+// console.log(myCurrentJob);
+
+
+
+//This is for the assignment 01 Lecture : values and variables
+let myCountry = 'India';
+let myContinent = "Asia";
+let countryPopulation = '132crore';
+console.log('The country in which I live in is ' + myCountry);
+console.log('The country in which I live is in ' + myContinent + ' continent');
+console.log('The population of the country is ' + countryPopulation);
