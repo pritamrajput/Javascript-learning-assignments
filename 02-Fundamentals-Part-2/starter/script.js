04e25819dbf430b74a7c1cb7da9b1abd2b585982
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-09 21:58:54 +0530

    Strict mode lecture Done

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------
+
+// ********* Lecture 17 : Activate Strict Mode ************
+
+// -----------------------------------------------------------
+
+'use strict';
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive :D');
+
+const interface ='Audio'; // This will throw a syntax error as Javascript is reserving the variable 'interface' for a feature that it might add in the future. we see the error only because we have activated the strict mode :D ;
+const private = 3453; // This will throw a syntax error as Javascript is reserving the variable 'private' for a feature that it might add in the future. we see the error only because we have activated the strict mode :D ;
