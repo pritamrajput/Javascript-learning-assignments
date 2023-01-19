6ad19a5231427318f5d7954c75cd902428570c94
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-19 20:20:21 +0530

    New section started

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 35;
+if(x === 35) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+console.log();
