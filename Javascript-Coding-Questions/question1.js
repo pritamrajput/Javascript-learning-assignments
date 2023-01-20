435aa97ab82b38e47fa633adc5575ee7dce08c93
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-20 12:18:04 +0530

    Basic question 1 Done

---
+++ Javascript-Coding-Questions/question1.js
@@ -0,0 +1,27 @@
+// Codewars basic questions solution
+function getLength(arr){
+    //return length of arr
+    const len = arr.length;
+    return len;
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    const firstElement = arr.shift();
+    return firstElement;
+  }
+  function getLast(arr){
+    //return the last element of arr
+    const lastElement = arr.pop();
+    return lastElement;
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    arr.push('1')
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    const popElement = arr.pop();
+    return arr;
+  }  
