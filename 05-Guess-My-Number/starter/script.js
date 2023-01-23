86b7182354dacc52f73df66fcc97431e3e1180ba
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-23 19:33:02 +0530

    implementing the Game logic lecture done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,6 +10,13 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random()*20 )+1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+// IMP NOTE : We could have also stored score in the DOM and to do that we just read the value right from the DOM and than we decrease the value and than write back to the DOM but then we would not have the value in our Code. so our application would not have any way of knowing the score at all point of time. so its always good to have a variable which stores the data in our code and not just rely on the DOM to hold our Data.
+//The variable "score" is also called a state variable. because this variable is a part of application state, which is all the data that is relevant to the application.
+
 document.querySelector('.check').addEventListener('click', function (){
 
     const guess = Number(document.querySelector('.guess').value);
@@ -17,5 +24,32 @@ document.querySelector('.check').addEventListener('click', function (){
 
     if(!guess){
         document.querySelector('.message').textContent = '⛔ No Number';
+    }
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number!'; 
+    }
+    else if(guess > secretNumber){
+       if(score>1){
+        document.querySelector('.message').textContent = '📈 Too High!';
+        score--;
+        document.querySelector(".score").textContent = score;
+       }
+      else{
+        document.querySelector('.message').textContent = '🔥you lost the Game!';
+        document.querySelector(".score").textContent = '0';
+      }  
+    }
+    else if(guess < secretNumber){
+       
+        if(score>1){
+            document.querySelector('.message').textContent = '📉 Too Low!'; 
+            score--;
+            document.querySelector(".score").textContent = score;
+           }
+          else{
+            document.querySelector('.message').textContent = '🔥you lost the Game!';
+            document.querySelector(".score").textContent = '0';
+          }  
+
     }
 })
