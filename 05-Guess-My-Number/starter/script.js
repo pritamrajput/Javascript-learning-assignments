dfb2427d22b6ce80c4427815fd1ce8ac040130f6
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-24 15:20:31 +0530

    code refactoring Done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,6 +15,10 @@ let secretNumber = Math.trunc(Math.random()*20 )+1;
 let score = 20;
 
 let highscore = 0;
+
+const callMessage = (message) => {
+    document.querySelector('.message').textContent = message;
+}
 // IMP NOTE : We could have also stored score in the DOM and to do that we just read the value right from the DOM and than we decrease the value and than write back to the DOM but then we would not have the value in our Code. so our application would not have any way of knowing the score at all point of time. so its always good to have a variable which stores the data in our code and not just rely on the DOM to hold our Data.
 //The variable "score" is also called a state variable. because this variable is a part of application state, which is all the data that is relevant to the application.
 
@@ -24,7 +28,8 @@ document.querySelector(".again").addEventListener('click', ()=>{
     //console.log("Hello world");
     secretNumber = Math.trunc(Math.random()*20 )+1;
     document.querySelector("body").style.backgroundColor = '#222'
-    document.querySelector('.message').textContent = 'Start guessing...';
+    callMessage('Start guessing...');
+    // document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('.score').textContent = '20';
     document.querySelector('.number').style.width = '15rem';
     document.querySelector('.number').textContent = '?';
@@ -38,12 +43,14 @@ document.querySelector('.check').addEventListener('click', function (){
     
     //When no number
     if(!guess){
-        document.querySelector('.message').textContent = '⛔ No Number';
+        callMessage('⛔ No Number');
+        //document.querySelector('.message').textContent = '⛔ No Number';
     }
 
     //When a player wins
     else if(guess === secretNumber){
-        document.querySelector('.message').textContent = '🎉 Correct Number!'; 
+        callMessage('🎉 Correct Number!');
+        // document.querySelector('.message').textContent = '🎉 Correct Number!'; 
         const body = document.querySelector("body");
 
         document.querySelector('.number').textContent = secretNumber;
@@ -58,32 +65,46 @@ document.querySelector('.check').addEventListener('click', function (){
             document.querySelector('.highscore').textContent = highscore;
         }
     }
-
-    //When number is too high
-    else if(guess > secretNumber){
-       if(score>1){
-        document.querySelector('.message').textContent = '📈 Too High!';
-        score--;
-        document.querySelector(".score").textContent = score;
-       }
-      else{
-        document.querySelector('.message').textContent = '🔥you lost the Game!';
-        document.querySelector(".score").textContent = '0';
-      }  
-    }
-
-    //When number is too low
-    else if(guess < secretNumber){
-       
+    // when guess is wrong  
+    else if(guess !== secretNumber){
         if(score>1){
-            document.querySelector('.message').textContent = '📉 Too Low!'; 
+            callMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+            // document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too High!' : '📉 Too Low!';
             score--;
             document.querySelector(".score").textContent = score;
            }
           else{
-            document.querySelector('.message').textContent = '🔥you lost the Game!';
+            callMessage('🔥you lost the Game!');
+           // document.querySelector('.message').textContent = '🔥you lost the Game!';
             document.querySelector(".score").textContent = '0';
-          }  
-
+          } 
     }
+
+    //When number is too high
+    // else if(guess > secretNumber){
+    //    if(score>1){
+    //     document.querySelector('.message').textContent = '📈 Too High!';
+    //     score--;
+    //     document.querySelector(".score").textContent = score;
+    //    }
+    //   else{
+    //     document.querySelector('.message').textContent = '🔥you lost the Game!';
+    //     document.querySelector(".score").textContent = '0';
+    //   }  
+    // }
+
+    //When number is too low
+    // else if(guess < secretNumber){
+       
+    //     if(score>1){
+    //         document.querySelector('.message').textContent = '📉 Too Low!'; 
+    //         score--;
+    //         document.querySelector(".score").textContent = score;
+    //        }
+    //       else{
+    //         document.querySelector('.message').textContent = '🔥you lost the Game!';
+    //         document.querySelector(".score").textContent = '0';
+    //       }  
+
+    // }
 })
