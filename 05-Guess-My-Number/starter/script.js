6e1ab5047064948b1355ae6754d53f70212062ef
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-24 12:41:20 +0530

    Guess Game project almost done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,12 +10,25 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random()*20 )+1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random()*20 )+1;
+
 
 let score = 20;
 // IMP NOTE : We could have also stored score in the DOM and to do that we just read the value right from the DOM and than we decrease the value and than write back to the DOM but then we would not have the value in our Code. so our application would not have any way of knowing the score at all point of time. so its always good to have a variable which stores the data in our code and not just rely on the DOM to hold our Data.
 //The variable "score" is also called a state variable. because this variable is a part of application state, which is all the data that is relevant to the application.
+
+// Game resetting logic
+
+document.querySelector(".again").addEventListener('click', ()=>{
+    //console.log("Hello world");
+    secretNumber = Math.trunc(Math.random()*20 )+1;
+    document.querySelector("body").style.backgroundColor = '#222'
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = '20';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+})
 
 document.querySelector('.check').addEventListener('click', function (){
 
@@ -32,9 +45,11 @@ document.querySelector('.check').addEventListener('click', function (){
         document.querySelector('.message').textContent = '🎉 Correct Number!'; 
         const body = document.querySelector("body");
 
+        document.querySelector('.number').textContent = secretNumber;
+
     //Note : whenever we manipulating style with the help of javascript we always specify a string.
         body.style.backgroundColor = '#60b347' ;
-    // Note : The styles we apply using javascript are "inline styles"
+    // Note : The styles we apply using javascript are "inline style"
         document.querySelector(".number").style.width = '30rem';
     }
 
