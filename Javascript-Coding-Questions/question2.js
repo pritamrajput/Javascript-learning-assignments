fcffb56cae8eb72140f55791ecc6ef3f9f6b65f2
Pritam Chauhan <rajputpritam619@gmail.com>
2023-02-03 10:04:13 +0530

    Leetcode Que solved

--- Javascript-Coding-Questions/question2.js
+++ Javascript-Coding-Questions/question2.js
@@ -1,7 +1,8 @@
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
-//  Finding mean form two arrays Leetcode
+ * @return {number}
+ */
 var findMedianSortedArrays = function(nums1, nums2) {
     let median;
     let newArr = nums1.concat(nums2);
@@ -16,11 +17,11 @@ var findMedianSortedArrays = function(nums1, nums2) {
     }
     if(newArr.length % 2 === 0){
         let num = Math.trunc(newArr.length / 2);
-        median = (newArr[num] + newArr[num+1])/2
+        median = (newArr[num] + newArr[num-1])/2
     }
     else{
         let num = Math.trunc(newArr.length / 2);
-        median = newArr[num + 1];
+        median = newArr[num];
     }
     return median;
 };
