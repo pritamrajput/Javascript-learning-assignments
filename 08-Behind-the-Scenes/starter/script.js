efbc342c3a3417c78f42ecfe7a32a1a296dcacb7
Pritam Chauhan <rajputpritam619@gmail.com>
2023-02-05 21:12:59 +0530

    Scoping in Practice lecture done

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,7 +4,25 @@ function calcAge(birthYear){
     const age = 2037 - birthYear;
  
     function printAge(){
-        const output = `${firstName} you are ${age}, born in ${birthYear}`;
+        let output = `${firstName} you are ${age}, born in ${birthYear}`;
+        console.log(output);
+        if(birthYear >= 1981 && birthYear <= 1996){
+            var millenial = true
+             
+            // Creating NEW variable with the same name as outer scope's variable 
+            const firstName = 'steven';
+
+            // REASSIGNING outer scope's variable
+            output = 'NEW OUTPUT!';
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b){
+                return a+b;
+            }
+            console.log(add(2,3)); //Note:- This proves function are block scoped starting from ES6 but only when we use strict mode.
+        }
+        console.log(millenial);
         console.log(output);
     }
     printAge();
