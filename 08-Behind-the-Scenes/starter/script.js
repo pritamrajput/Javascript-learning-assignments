ec887a1e812cc2d29d3c022c760ba3d04bee141c
Pritam Chauhan <rajputpritam619@gmail.com>
2023-02-06 21:08:54 +0530

    Hoisting files added

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -31,3 +31,31 @@ function calcAge(birthYear){
 
 const firstName = "jonas";
 calcAge(1992);
+
+// Lecture 95: Hoisting in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2,3));
+console.log(addExpr(2,3));
+console.log(addArrow(2,3));
+
+function addDecl(a,b){
+    return a + b;
+}
+
+var addExpr = function(a,b){
+    return a + b;
+}
+
+var addArrow = () => {
+    return a + b;
+}
