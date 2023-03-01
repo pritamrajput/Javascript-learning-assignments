9831a9c2d80fd6507cf556236c2ae4e2f72e62a0
Pritam Chauhan <rajputpritam619@gmail.com>
2023-03-01 21:26:08 +0530

    ways to create an array in javascript

---
+++ Javascript-Coding-Questions/Arrays/question1.js
@@ -0,0 +1,11 @@
+'use strict';
+// Ways to create array in javascript
+let z = [1,4,5,345,5,6];
+let x = new Array(1,3,4,4,3,2);
+console.log(x);
+
+let y = Array.of(4,4,5,2,2);
+console.log(Array.of(4,4,5,2,2)[0]);
+
+let str = "My/na/m,ei,sP,ritam";
+console.log(str.split(""))
