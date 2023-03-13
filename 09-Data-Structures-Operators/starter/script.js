2e1a179826bf019ad70d1c1fcef038e36eacf162
Pritam Chauhan <rajputpritam619@gmail.com>
2023-03-13 22:15:48 +0530

    for of loop done

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -300,3 +300,89 @@ rest2.owner &&= '<Anonymous>';
 
 console.log(rest1);
 console.log(rest2);
+
+
+// Coding Challenge #1
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+
+  printGoals : function(...recievedPlayers){
+console.log(recievedPlayers);
+  }
+};
+
+
+
+const [players1, players2] = game.players;
+console.log(players1,players2);
+const [gk1, ...fieldPlayers1]  = players1;
+console.log(gk1, fieldPlayers1);
+
+const [gk2, ...fieldPlayers2]  = players2;
+console.log(gk2, fieldPlayers2);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1,'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// const {team1, x, team2} = game.odds; //One way
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1,draw,team2);
+
+game.printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+game.printGoals(...game.scored);
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu){
+  console.log(item);
+}
+
+for(const item of menu.entries()){
+  console.log(item);
+}
+
+console.log([...menu.entries()]);
+
+for(const [i, el] of menu.entries()){
+  console.log(`${i + 1}: ${el}`);
+}
