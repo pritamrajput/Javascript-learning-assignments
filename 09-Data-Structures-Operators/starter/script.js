78f2fa045bb93f6636caa23306734d5445a69543
Pritam Chauhan <rajputpritam619@gmail.com>
2023-03-18 11:55:14 +0530

    coding challege done

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -717,3 +717,53 @@ console.log(`we have ${n} planes in line ${'✈️'.repeat(n)}`);
 
 planeInline(3);
 planeInline(5)
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅ 
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+const text = document.querySelector('textarea').value;
+
+const btn = document.querySelector('button');
+btn.addEventListener('click',function(){
+ 
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  // My way of solvng the problem
+  // for(let i = 0;i<rows.length;i++){
+  //   const [firstHalf, secondHalf] = rows[i].toLowerCase().split("_");
+  //   const s = secondHalf[0].toUpperCase() + secondHalf.slice(1);
+  //   const f = firstHalf.trim();
+  //   console.log(`${[f,s].join("").padEnd(20)}${'✅'.repeat(i + 1)}`);
+  // }
+
+  // Jonas way
+
+  for(const [i,row] of rows.entries()){
+    const[first,second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(second[0],second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+})
