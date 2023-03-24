4359b68483b0604b1da16084c8f50dbcf838bf5d
Pritam Chauhan <rajputpritam619@gmail.com>
2023-03-24 09:43:12 +0530

    new files added

---
+++ Namaste-Javascript/Episode-8/script.js
@@ -0,0 +1,8 @@
+'use strict';
+let a = 10;
+console.log(a);
+let f  = ()=>{
+    console.log("Hello world!");
+}
+f();
+var b =100;
---
+++ Namaste-Javascript/Episode-9/script.js
@@ -0,0 +1,24 @@
+// BLOCK
+// let b = 100
+// {
+//     // This is the block 
+//    // compound statement
+//    var a = 10;
+//    let b = 20;
+//    const c = 30;
+//    console.log(a);
+//    console.log(b);
+//    console.log(c);
+// }
+// if(true)console.log("hello world");
+// console.log(b);
+//    console.log(b);
+//    console.log(c);
+const a = 200;
+{
+    const a = 10;
+{
+const a = 20;
+console.log(a);
+}
+}
