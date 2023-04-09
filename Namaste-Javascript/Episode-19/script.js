b463f56d936eea294c0ee96754cbccdd32fa33d9
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-09 15:06:30 +0530

    episode 19 conti..

--- Namaste-Javascript/Episode-19/script.js
+++ Namaste-Javascript/Episode-19/script.js
@@ -39,44 +39,73 @@ const arr = [5,1,3,2,6];
 // console.log(output2);
 
 //sum or max without using reduce method
-function arrSum(arr){
- let sum = 0;
- for(let i = 0;i<arr.length;i++){
-    sum =  sum + arr[i];
- }
- return sum;
-}
+// function arrSum(arr){
+//  let sum = 0;
+//  for(let i = 0;i<arr.length;i++){
+//     sum =  sum + arr[i];
+//  }
+//  return sum;
+// }
 
-console.log(arrSum(arr));
+// console.log(arrSum(arr));
 
-// Sum of element inside the array with REDUCE
-const output = arr.reduce(function(acc,curr){
-    acc = acc + curr;
-    return acc;
-},0)
+// // Sum of element inside the array with REDUCE
+// const output = arr.reduce(function(acc,curr){
+//     acc = acc + curr;
+//     return acc;
+// },0)
+// console.log(output);
+
+// // To find max value from the given array wihtout reduce method
+
+// function findMax(arr){
+//     let max = 0;
+//     for(let i = 0;i<arr.length;i++){
+//         if(arr[i]>max){
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+// console.log(findMax(arr));
+
+// // Find max with reduce method
+
+// const output1 = arr.reduce(function(acc,curr){
+//     if(curr>acc){
+//         acc = curr
+//     }
+//     return acc;
+// },0)
+
+// console.log(output1);
+
+const users = [
+    {firstName:"akshay",lastName:"saini",age:26},
+    {firstName:"donald",lastName:"trump",age:75},
+    {firstName:"elon",lastName:"musk",age:50},
+    {firstName:"deepika",lastName:"padukone",age:26
+},
+];
+
+// obtaining array of fullname from the array users
+const fullName = users.map((x)=>{
+    return `${x.firstName} ${x.lastName}`
+})
+
+console.log(fullName);
+
+// acc = {26:2, 75:1, 50:1} solve this
+
+const output =  users.reduce(function(acc,curr){
+  if(acc[curr.age]){
+    acc[curr.age] = ++acc[curr.age];
+  }
+  else{
+    acc[curr.age] = 1;
+  }
+
+  return acc;
+},{})
 console.log(output);
-
-// To find max value from the given array wihtout reduce method
-
-function findMax(arr){
-    let max = 0;
-    for(let i = 0;i<arr.length;i++){
-        if(arr[i]>max){
-            max = arr[i];
-        }
-    }
-    return max;
-}
-
-console.log(findMax(arr));
-
-// Find max with reduce method
-
-const output1 = arr.reduce(function(acc,curr){
-    if(curr>acc){
-        acc = curr
-    }
-    return acc;
-},0)
-
-console.log(output1);
