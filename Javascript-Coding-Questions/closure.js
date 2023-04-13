1def02845b9ee3e65f28003343c5f34d23f29a55
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-13 23:13:04 +0530

    callback hell done

--- Javascript-Coding-Questions/closure.js
+++ Javascript-Coding-Questions/closure.js
@@ -1,9 +1,26 @@
-function x(){
-    const i =1;
-    for(var r = 1;i<=5;r++){
-        setTimeout(function(){
-            console.log(r);
-        },r*1000);
-    }
+// function x(){
+//     const i =1;
+//     for(var r = 1;i<=5;r++){
+//         setTimeout(function(){
+//             console.log(r);
+//         },r*1000);
+//     }
+// }
+// x();
+
+const orderPizza =()=>{
+    console.log("calling Pizza shop...");
+    setTimeout(()=>{
+        console.log("call picked and order given🎉");
+        console.log("wait for order to arrive");
+        setTimeout(()=>{
+            console.log("order arrived...");
+            setTimeout(()=>{
+               console.log("Payment done");
+               console.log("You can eat Pizza🥳");
+            },3000)
+        },3000)
+    },3000)
 }
-x();
+
+orderPizza();
