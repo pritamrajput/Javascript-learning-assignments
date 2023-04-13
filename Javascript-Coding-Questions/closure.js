9a69ff4e0a51bf36beff509ee647c5b291a58149
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-13 23:33:12 +0530

    callback hell

--- Javascript-Coding-Questions/closure.js
+++ Javascript-Coding-Questions/closure.js
@@ -8,19 +8,40 @@
 // }
 // x();
 
-const orderPizza =()=>{
-    console.log("calling Pizza shop...");
+// const orderPizza =()=>{
+//     console.log("calling Pizza shop...");
+//     setTimeout(()=>{
+//         console.log("call picked and order given🎉");
+//         console.log("wait for order to arrive");
+//         setTimeout(()=>{
+//             console.log("order arrived...");
+//             setTimeout(()=>{
+//                console.log("Payment done");
+//                console.log("You can eat Pizza🥳");
+//             },3000)
+//         },3000)
+//     },3000)
+// }
+// orderPizza();
+
+const orderPizza = (giveOrder)=>{
+    console.log("calling pizza shop...");
     setTimeout(()=>{
-        console.log("call picked and order given🎉");
-        console.log("wait for order to arrive");
-        setTimeout(()=>{
-            console.log("order arrived...");
-            setTimeout(()=>{
-               console.log("Payment done");
-               console.log("You can eat Pizza🥳");
-            },3000)
-        },3000)
+        giveOrder(orderPlaced);
+    },3000);
+}
+
+function giveOrder(orderPlaced){
+    console.log("call picked");
+    console.log("Please place your order");
+    setTimeout(()=>{
+      orderPlaced();    
     },3000)
 }
 
-orderPizza();
+function orderPlaced(){
+    console.log("order received");
+    console.log("you can eat pizza");
+}
+
+orderPizza(giveOrder);
