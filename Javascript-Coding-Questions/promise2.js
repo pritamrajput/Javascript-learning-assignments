82eb56ad9fd65e416ee84b8e551b487d2ad62282
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-14 23:11:52 +0530

    promise made

--- Javascript-Coding-Questions/promise2.js
+++ Javascript-Coding-Questions/promise2.js
@@ -1,5 +1,41 @@
 'use strict';
 function getweather(){
-    return new Promise(function(resolve,reject){})
+    return new Promise(function(resolve,reject){
+        // async logic
+        setTimeout(()=>{
+            resolve("cloudy");
+        },2000)
+    })
 }
-console.log(getweather());
+
+
+function getWeatherIcon(weather){
+    return new Promise(function(resolve,reject){
+       switch(weather){
+        case "sunny":
+            resolve('🌞');
+            break;
+        case "cloudy":
+            resolve('☁️');
+            break;
+        case "rainy":
+            resolve("🌧️");
+            break;
+        default:
+            reject("ERROR NOT FOUND⚠️");
+       }
+    })
+}
+
+const onSuccess = function(data){
+    console.log(`First param ${data}`);
+}
+
+const onFailure =  function(data){
+    console.log(`Second param ${data}`);
+}
+
+let promise = getweather();
+promise
+.then(getWeatherIcon)
+.then(onSuccess,onFailure)
