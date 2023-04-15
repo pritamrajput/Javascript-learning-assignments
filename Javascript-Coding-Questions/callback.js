08aa9b2c3c86a5590f2dd2c455aa9ea63e6b8615
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-15 17:39:40 +0530

    callbakc

---
+++ Javascript-Coding-Questions/callback.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function pizzaArrived(){
+    console.log("Pizza arrived, you can eat pizza");
+}
+
+function whileWorking(){
+    console.log("Feeling hungry while working");
+ 
+    console.log("Ordering pizza");
+
+    console.log("Work conti...");
+
+    // setTimeout(pizzaArrived,3000);
+}
+
+whileWorking();
