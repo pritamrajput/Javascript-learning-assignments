71990c44dacd7d95b2223d59efd9dee89a5e61d0
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-16 23:59:48 +0530

    async await

---
+++ Javascript-Coding-Questions/async-await-2.js
@@ -0,0 +1,17 @@
+'use strict';
+// const url = 'http://worldtimeapi.org/api/timezone/Asia/Kolkata';
+
+// async function getData(){
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+// }
+// getData()
+
+// async function getData2(){
+//     const data2 = await getData().json();
+//     console.log(data2);  
+// }
+
+// getData2();
+const url = 'https://gnews.io/api/v4/top-headlines?category=general&apikey=322e4ebc46b02dbf4021701294353c25';
